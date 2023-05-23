32af19618f564c26580d111628047a45f4df1382
Salomão Santana <salomao.nascimento@muralis.com.br>
2023-05-23 17:55:23 -0300

    button advanced filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { ValidationComponent } from './pages/validation/validation.component';
 import { FilterBarComponent } from './pages/validation/components/filter-bar/filter-bar.component';
-import { ImageGalleryComponent } from './components/image-gallery/image-gallery.component';
 import { ActionsComponent } from './pages/validation/components/actions/actions.component';
 import { TransitListComponent } from './pages/validation/components/transit-list/transit-list.component';
 
+import { ImageGalleryComponent } from './components/image-gallery/image-gallery.component';
+import { ButtonComponent } from './components/button/button.component';
+
 @NgModule({
-  declarations: [AppComponent, ValidationComponent, FilterBarComponent, ImageGalleryComponent, ActionsComponent, TransitListComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    ValidationComponent,
+    FilterBarComponent,
+    ImageGalleryComponent,
+    ActionsComponent,
+    TransitListComponent,
+    ButtonComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
